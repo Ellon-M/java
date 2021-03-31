e5d8983e29c40a7d2bb237b4849c12fc929e4182
Ellon Mordecai <67250802+Ellon-M@users.noreply.github.com>
2021-03-31 06:22:01 +0300

    Class Encapsulation

---
+++ classEncapsulation/AccessModifiers.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classEncapsulation;
+
+/**
+ *
+ * @author ellon
+ */
+public class AccessModifiers {
+    // the wrapping up of data and functions into one single unit
+    // where the data is not accessible to the outside world, and 
+    // only the functions which are wrapped (encapsulated) in the class can access it.
+    
+    // process of hiding object implementation, from another object, presenting only the interfaces that should be visible
+    
+    
+    // Encapsulation is achieved in classes by:
+             //- visibility modifiers         - members must be declared with the minimum level of visibility
+             //- accessor and mutator methods - getters and setters 
+    
+ 
+    
+    public static void main(String[] args) {
+        
+        // instantiate the class
+        Wave soundWave = new Wave();
+        
+        // for the public member of the class
+        soundWave.frequency = 13000;
+        
+        System.out.println("Frequency:" + soundWave.frequency);
+        
+        // for the private member of the class
+        // (we use getters and setters to access the variable, since its access is restricted to other classes)
+
+        soundWave.setWaveForm("Saw Tooth");
+        
+        // after setting a new wave form
+        // the get method is used to call it
+        System.out.println("Wave Form: " + soundWave.getWaveForm());
+               
+    }
+    
+}
+
+class Wave {
+    
+    // visibility modifiers (access modifiers)
+    
+    public int frequency;       // - public (default) 
+    private String waveForm;    // - private
+    protected int duration;     // - protected
+    
+    
+    
+    // public -  the code is accessible to all other classes
+    // private -      "   "     "       within its declared class
+    // protected - the code is accessible within the declared class and its subclasses but in the same package
+    
+    // to extend the visibility of the private member to other classes
+    // getter method..
+    public String getWaveForm(){
+        return waveForm;
+    }
+    
+    // and
+    public void setWaveForm(String newWaveForm){    // setter method
+        this.waveForm = newWaveForm;
+    }
+} 
+
---
+++ classEncapsulation/NonAccessModifiers.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classEncapsulation;
+
+/**
+ *
+ * @author ellon
+ */
+public class NonAccessModifiers {
+    // these modifiers are not used to control access to classes,
+    // but they provide other functionality..
+    // they describe how a class can be accessed
+    
+    // they are usually used during class inheritance
+    
+    public static void main(String[] args) {
+        
+        // trying to override a variable declared final will bring an error
+        // Accountant.age = 19;
+    }
+    
+}
+// 1. Final
+
+
+// the final keyword prevents this class from being inherited by other classes
+final class Account {
+    int balance = 30000;
+    int accNo = 101010;
+}
+
+// the final keyword also prevents attributes and methods from being overriden
+class Accountant {
+    final String name = "Viggy";
+    final int age  = 20;
+    final int id = 232323;
+}
+
+// 2. Abstract
+
+// the abstract keyword is used in classes that cannot/ are not intended to create objects
+// to access abstract classes, inheritance comes to play
+abstract class Finance{
+    // trying to create an object from the abstract class will bring an error
+    // Finance interFinance = new Finance();
+
+}
+
+// the abstract keyword is also used in methods, but the method declared does not have a body
+// the body is declared in the subclass ( a class that inherits from it )
+// the class containing abstract methods must also be abstract
+abstract class Yesterday {
+ public abstract void study();   
+}
